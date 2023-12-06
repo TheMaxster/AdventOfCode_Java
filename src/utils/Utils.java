89d99f9d74e5548797323b7d4ecbd67d520aa9b8
Max Glembock <max.glembock@mgm-tp.com>
2023-12-06 13:06:35 +0100

    Solved day 06

---
+++ src/utils/Utils.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+
+    public static void log(String logStatement){
+        System.out.println(logStatement);
+    }
+}
