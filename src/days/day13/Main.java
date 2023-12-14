8288e50efa5dfa2240b1f65a53c0b46ccea30384
Max Glembock <max.glembock@mgm-tp.com>
2023-12-14 07:29:43 +0100

    Cleaned up day 13

--- src/days/day13/Main.java
+++ src/days/day13/Main.java
@@ -1,6 +1,7 @@
 package days.day13;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +16,7 @@ public class Main {
         //  final String filePath = System.getProperty("user.dir") + "/resources/days/day13/input_13_test_01_01.txt";
         //  final String filePath = System.getProperty("user.dir") + "/resources/days/day13/input_13_test_01_02.txt";
         //final String filePath = System.getProperty("user.dir") + "/resources/days/day13/input_13_test_02_01.txt";
-            final String filePath = System.getProperty("user.dir") + "/resources/days/day13/input_13.txt";
+        final String filePath = System.getProperty("user.dir") + "/resources/days/day13/input_13.txt";
 
         List<String> inputs = ImportUtils.readAsList(filePath);
 
@@ -26,7 +27,7 @@ public class Main {
 
         for (String[][] array : inputsAsArray) {
             print(array);
-           //       ReflectionTuple tuple = calculatePart1(array, null);
+          //  ReflectionTuple tuple = calculatePart1(array, null);
             ReflectionTuple tuple = calculatePart2(array);
 
             if (tuple.horizontalReflection() != null) {
@@ -37,8 +38,8 @@ public class Main {
 
         }
 
-        int totalHorizontalReflections = horizontalReflections.stream().map(Reflection::line).reduce((a, b) -> a + b).orElse(0);
-        int totalVerticalReflections = verticalReflections.stream().map(Reflection::line).reduce((a, b) -> a + b).orElse(0);
+        int totalHorizontalReflections = horizontalReflections.stream().map(Reflection::line).reduce(Integer::sum).orElse(0);
+        int totalVerticalReflections = verticalReflections.stream().map(Reflection::line).reduce(Integer::sum).orElse(0);
 
         Utils.log("Total horizontal reflections: " + totalHorizontalReflections);
         Utils.log("Total vertical reflections: " + totalVerticalReflections);
@@ -48,8 +49,42 @@ public class Main {
 
     }
 
-    private record ReflectionTuple(Reflection horizontalReflection, Reflection verticalReflection) {
+    private static List<String[][]> convertInputInArrays(final List<String> inputs) {
+        List<String[][]> inputsAsArray = new ArrayList<>();
 
+        List<String> tmpArrayList = new ArrayList<>();
+        for (String input : inputs) {
+
+            if (input.isBlank()) {
+
+                if (!tmpArrayList.isEmpty()) {
+                    String[][] array = convertListToArray(tmpArrayList);
+                    inputsAsArray.add(array);
+                }
+
+                tmpArrayList = new ArrayList<>();
+            } else {
+                tmpArrayList.add(input);
+            }
+        }
+
+        return inputsAsArray;
+    }
+
+    private static String[][] convertListToArray(List<String> list) {
+        return list.stream()
+                .map(element -> element.split(""))
+                .toArray(String[][]::new);
+    }
+
+    public static void print(String[][] array) {
+        for (String[] row : array) {
+            for (String element : row) {
+                System.out.print(element);
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
     }
 
     private static ReflectionTuple calculatePart2(
@@ -71,9 +106,7 @@ public class Main {
                         continue;
                     }
 
-               //     if (!areReflectionTuplesEqual(ogTuple, tuple)) {
-                        return tuple;
-                 //   }
+                    return tuple;
                 } else if (Objects.equals(array[i][j], ".")) {
                     deepCopy[i][j] = "#";
                     ReflectionTuple tuple = calculatePart1(deepCopy, ogReflectionToAvoid);
@@ -83,61 +116,12 @@ public class Main {
                         continue;
                     }
 
-                 //   if (!areReflectionTuplesEqual(ogTuple, tuple)) {
-                        return tuple;
-                 //   }
+                    return tuple;
                 }
             }
         }
-        //return new ReflectionTuple(null,null);
+
         throw new IllegalStateException("We should have find a tuple!");
-    }
-
-    private static boolean areReflectionTuplesEqual(
-            ReflectionTuple rt1,
-            ReflectionTuple rt2
-    ) {
-        //        if(rt1.horizontalReflection() == null && rt2.horizontalReflection() != null
-        //         || rt1.horizontalReflection() != null && rt2.horizontalReflection() == null
-        //        ){
-        //            return false;
-        //        }
-        //
-        //        if(rt1.verticalReflection() == null && rt2.verticalReflection() != null
-        //        || rt1.verticalReflection() != null && rt2.verticalReflection() == null
-        //
-        //        ){
-        //            return false;
-        //        }
-
-        return areReflectionsEquals(rt1.horizontalReflection(), rt2.horizontalReflection())
-                && areReflectionsEquals(rt1.verticalReflection(), rt2.verticalReflection());
-    }
-
-    private static boolean areReflectionsEquals(
-            Reflection ref1,
-            Reflection ref2
-    ) {
-        if ((ref1 == null && ref2 != null) || (ref1 != null && ref2 == null)) {
-            return false;
-        }
-
-        if (ref1 == null && ref2 == null) {
-            return true;
-        }
-
-        return ref1.reflectionLines() == ref2.reflectionLines() && ref1.line() == ref2.line() && ref1.direction() == ref2.direction();
-    }
-
-    public static String[][] deepCopy2Array(String[][] original) {
-        String[][] copy = new String[original.length][];
-        for (int i = 0; i < original.length; i++) {
-            copy[i] = new String[original[i].length];
-            for (int j = 0; j < original[i].length; j++) {
-                copy[i][j] = original[i][j];
-            }
-        }
-        return copy;
     }
 
     private static ReflectionTuple calculatePart1(
@@ -157,31 +141,6 @@ public class Main {
         //        }
 
         return new ReflectionTuple(horizontalReflection, verticalReflection);
-    }
-
-    public static void print(String[][] array) {
-        for (String[] row : array) {
-            for (String element : row) {
-                System.out.print(element);
-            }
-            System.out.println(" ");
-        }
-        System.out.println(" ");
-    }
-
-    public static String[][] transpose(String[][] original) {
-        int numRows = original.length;
-        int numCols = original[0].length;
-
-        String[][] transposed = new String[numCols][numRows];
-
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                transposed[j][i] = original[i][j];
-            }
-        }
-
-        return transposed;
     }
 
     private static Reflection calculateReflections(
@@ -223,19 +182,13 @@ public class Main {
                         && reflectionToAvoid.reflectionLines() == reflectionCount
                         && Objects.equals(reflectionToAvoid.direction(), direction);
 
-                if (!doWeWantToAvoidFoundReflection){
+                if (!doWeWantToAvoidFoundReflection) {
                     splitMap.put(splitAfterLine + 1, reflectionCount);
                     return new Reflection(splitAfterLine + 1, reflectionCount, direction);
                 }
             }
         }
 
-        //        if (splitMap.isEmpty()) {
-        //            return null;
-        //        } else {
-        //            Map.Entry<Integer, Integer> maxEntry = Collections.max(splitMap.entrySet(), Map.Entry.comparingByKey());
-        //            return new Reflection(maxEntry.getKey(), maxEntry.getValue());
-        //        }
         return null;
     }
 
@@ -253,45 +206,38 @@ public class Main {
         return areEqual;
     }
 
-    //    boolean areEqual = true;
-    //        for (int i = 0; i < array1.length; i++) {
-    //        if (!array1[i].equals(array2[i])) {
-    //            System.out.println("Unterschied bei Index " + i + ": \"" + array1[i] + "\" vs \"" + array2[i] + "\"");
-    //            areEqual = false;
-    //        }
-    //    }
+    public static String[][] transpose(String[][] original) {
+        int numRows = original.length;
+        int numCols = original[0].length;
 
-    private record Reflection(int line, int reflectionLines, String direction) {
+        String[][] transposed = new String[numCols][numRows];
 
-    }
-
-    private static List<String[][]> convertInputInArrays(final List<String> inputs) {
-        List<String[][]> inputsAsArray = new ArrayList<>();
-
-        List<String> tmpArrayList = new ArrayList<>();
-        for (String input : inputs) {
-
-            if (input.isBlank()) {
-
-                if (!tmpArrayList.isEmpty()) {
-                    String[][] array = convertListToArray(tmpArrayList);
-                    inputsAsArray.add(array);
-                }
-
-                tmpArrayList = new ArrayList<>();
-            } else {
-                tmpArrayList.add(input);
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                transposed[j][i] = original[i][j];
             }
         }
 
-        return inputsAsArray;
+        return transposed;
     }
 
+    public static String[][] deepCopy2Array(String[][] original) {
+        String[][] copy = new String[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = new String[original[i].length];
+            for (int j = 0; j < original[i].length; j++) {
+                copy[i][j] = original[i][j];
+            }
+        }
+        return copy;
+    }
 
-    private static String[][] convertListToArray(List<String> list) {
-        return list.stream()
-                .map(element -> element.split(""))
-                .toArray(String[][]::new);
+    private record ReflectionTuple(Reflection horizontalReflection, Reflection verticalReflection) {
+
+    }
+
+    private record Reflection(int line, int reflectionLines, String direction) {
+
     }
 
 }
