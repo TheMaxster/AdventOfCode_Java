08a9cf6a9d97996fbc40030d5fe19f4291678d28
Max <max.glembock@googlemail.com>
2024-12-15 16:57:13 +0100

    Solved Day14 and Day15

--- src/main/java/year2024/day14/Day14.java
+++ src/main/java/year2024/day14/Day14.java
@@ -1,26 +1,160 @@
 package year2024.day14;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import application.Day;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import utils.Coordinate;
+import utils.ImportUtils;
 
 /**
  * See https://adventofcode.com/2024/day/1
  */
 public class Day14 extends Day {
 
-    public String part1(final List<String> input) {
-        //        final List<String> input = ImportUtils.readAsList(FILE_PATH);
-
-        return "test";
-    }
-
-    public String part2(final List<String> input) {
-        return "test";
-    }
+    private static final String FILE_PATH = "src/main/resources/year2024/day14/input_test_01.txt";
 
     @Override
     public Boolean getLoggingEnabled() {
-        return true;
+        return false;
     }
+
+    private static final int TEST_WIDTH = 11;
+    private static final int TEST_HEIGHT = 7;
+
+    private static final int WIDTH = 101; //TEST_WIDTH;
+    private static final int HEIGHT = 103;// TEST_HEIGHT;
+
+    private static final Pattern PATTERN = Pattern.compile("p=(-?\\d+),(-?\\d+) v=(-?\\d+),(-?\\d+)");
+
+    public String part1(final List<String> input) {
+        final List<String> importInput = ImportUtils.readAsList(FILE_PATH);
+
+        final int numberOfSeconds = 100;
+        final List<Robot> robots = parseInput(input);
+        final List<Robot> newRobots = calculateNewPositions(robots, WIDTH, HEIGHT, numberOfSeconds);
+
+        final int medianWidth = (WIDTH - 1) / 2;
+        final int medianHeight = (HEIGHT - 1) / 2;
+
+        final List<Integer> sectorResults = calculateSectors(newRobots, medianWidth, medianHeight);
+        int result = sectorResults.stream().reduce(1, (a, b) -> a * b);
+
+        return String.valueOf(result);
+    }
+
+    public String part2(final List<String> input) {
+        final List<Robot> robots = parseInput(input);
+
+        int numberOfSeconds = 0;
+        int maxSectorResult = 0;
+        List<Robot> newRobots = robots;
+
+        while (maxSectorResult < (robots.size() / 2)) { // We assume that more than half of the robots are needed to form the tree
+            numberOfSeconds++;
+            newRobots = calculateNewPositions(robots, WIDTH, HEIGHT, numberOfSeconds);
+
+            final int medianWidth = (WIDTH - 1) / 2;
+            final int medianHeight = (HEIGHT - 1) / 2;
+            maxSectorResult = calculateSectors(newRobots, medianWidth, medianHeight).stream().max(Integer::compareTo).get();
+        }
+
+        // Visual check
+        final String[][] map = new String[HEIGHT][HEIGHT];
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                map[i][j] = ".";
+            }
+        }
+        newRobots.forEach(robot -> {
+            map[robot.getX()][robot.getY()] = "#";
+        });
+        logMap(map);
+
+        return String.valueOf(numberOfSeconds);
+    }
+
+    private List<Robot> parseInput(final List<String> input) {
+        final List<Robot> robots = new ArrayList<>();
+        int id = 1;
+        for (final String s : input) {
+            final Matcher matcher = PATTERN.matcher(s);
+            if (matcher.matches()) {
+                final int x = Integer.parseInt(matcher.group(1));
+                final int y = Integer.parseInt(matcher.group(2));
+                final int dx = Integer.parseInt(matcher.group(3));
+                final int dy = Integer.parseInt(matcher.group(4));
+                final Robot robot = new Robot(id++, x, y, dx, dy);
+                robots.add(robot);
+            }
+        }
+
+        return robots;
+    }
+
+    private List<Integer> calculateSectors(
+            final List<Robot> robots,
+            final int medianWidth,
+            final int medianHeight
+    ) {
+        int sector1 = 0;
+        int sector2 = 0;
+        int sector3 = 0;
+        int sector4 = 0;
+
+        for (final Robot newRobot : robots) {
+            final int x = newRobot.getX();
+            final int y = newRobot.getY();
+            if (x < medianWidth && y < medianHeight) {
+                sector1++;
+            } else if (x > medianWidth && y < medianHeight) {
+                sector2++;
+            } else if (x < medianWidth && y > medianHeight) {
+                sector3++;
+            } else if (x > medianWidth && y > medianHeight) {
+                sector4++;
+            }
+        }
+
+        return List.of(sector1, sector2, sector3, sector4);
+
+    }
+
+    private List<Robot> calculateNewPositions(
+            final List<Robot> robots,
+            final int width,
+            final int height,
+            final int numberOfSeconds
+    ) {
+        final List<Robot> newRobots = new ArrayList<>();
+        for (final Robot robot : robots) {
+            final int newX = (robot.getX() + numberOfSeconds * robot.getDx()) % width;
+            final int newY = (robot.getY() + numberOfSeconds * robot.getDy()) % height;
+
+            final int correctedNewX = newX < 0 ? newX + width : newX;
+            final int correctedNewY = newY < 0 ? newY + height : newY;
+            final Robot newRobot = new Robot(robot.getId(), correctedNewX, correctedNewY, robot.getDx(), robot.getDy());
+            newRobots.add(newRobot);
+            log("Seconds " + numberOfSeconds + ": Id: " + newRobot.getId() + " -> " + newRobot.getX() + " " + newRobot.getY());
+        }
+        return newRobots;
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    private static class Robot {
+
+        int id;
+        int x;
+        int y;
+        int dx;
+        int dy;
+    }
+
+
 }
--- src/main/java/year2024/day15/Day15.java
+++ src/main/java/year2024/day15/Day15.java
@@ -1,26 +1,226 @@
 package year2024.day15;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import application.Day;
+import utils.ArrayUtils;
+import utils.Coordinate;
+import utils.ImportUtils;
 
 /**
  * See https://adventofcode.com/2024/day/1
  */
 public class Day15 extends Day {
 
-    public String part1(final List<String> input) {
-        //        final List<String> input = ImportUtils.readAsList(FILE_PATH);
-
-        return "test";
-    }
-
-    public String part2(final List<String> input) {
-        return "test";
-    }
+     private static final String FILE_PATH = "src/main/resources/year2024/day15/input_test_01.txt";
+   // private static final String FILE_PATH = "src/main/resources/year2024/day15/input_test_02.txt";
+ //   private static final String FILE_PATH = "src/main/resources/year2024/day15/input_test_03.txt";
 
     @Override
     public Boolean getLoggingEnabled() {
-        return true;
+        return false;
     }
+
+    public String part1(final List<String> input) {
+        final List<String> importInput = ImportUtils.readAsList(FILE_PATH);
+
+        final Pair<String[][], List<String>> parsedInput = parseInput(input);
+        final String[][] map = parsedInput.getLeft();
+        final List<String> instructions = parsedInput.getRight();
+
+        logMap(map);
+        log(instructions.toString());
+
+        final List<Coordinate> boxes = calculate(map, instructions);
+        log(boxes.toString());
+        final long sum = boxes.stream().mapToLong(box -> box.getX() * 100 + box.getY()).sum();
+
+        return String.valueOf(sum);
+
+    }
+
+    public String part2(final List<String> input) {
+        final List<String> importInput = ImportUtils.readAsList(FILE_PATH);
+
+        final Pair<String[][], List<String>> parsedInput = parseInput(input);
+        final String[][] map = parsedInput.getLeft();
+        final List<String> instructions = parsedInput.getRight();
+
+        final String[][] scaledUpMap = scaleUpMap(map);
+        logMap(scaledUpMap);
+        log(instructions.toString());
+
+        final List<Coordinate> boxes = calculateForPart2(scaledUpMap, instructions);
+        log(boxes.toString());
+        final long sum = boxes.stream().mapToLong(box -> box.getX() * 100 + box.getY()).sum();
+
+        return String.valueOf(sum);
+    }
+
+    private String[][] scaleUpMap(final String[][] map) {
+        final String[][] scaledUpMap = new String[map.length][2*map[0].length];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if(Objects.equals(map[i][j], "#")){
+                    scaledUpMap[i][2*j] = "#";
+                    scaledUpMap[i][2*j+1] = "#";
+                }else if(map[i][j].equals("O")){
+                    scaledUpMap[i][2*j] = "[";
+                    scaledUpMap[i][2*j+1] = "]";
+                } else if (map[i][j].equals(".")) {
+                    scaledUpMap[i][2*j] = ".";
+                    scaledUpMap[i][2*j+1] = ".";
+                } else if (map[i][j].equals("@")) {
+                    scaledUpMap[i][2*j] = "@";
+                    scaledUpMap[i][2*j+1] = ".";
+                }
+            }
+        }
+
+        return scaledUpMap;
+    }
+
+    private List<Coordinate> calculate(final String[][] map, final List<String> instructions) {
+        final List<List<Coordinate>> boxes = ArrayUtils.findAllOccurences(map, "O").stream().map(List::of).toList();
+        final Coordinate start = ArrayUtils.findAllOccurences(map, "@").get(0);
+        final List<Coordinate> frame = ArrayUtils.findAllOccurences(map, "#");
+
+        instructions.forEach(instruction -> {
+            move(start, boxes, frame, parseDirection(instruction));
+            log("Instruction: " + instruction);
+            if (getLoggingEnabled()) {
+                createAndLogMap(start, boxes, frame, map);
+            }
+        });
+
+        return boxes.stream().flatMap(Collection::stream).toList();
+    }
+
+    private List<Coordinate> calculateForPart2(final String[][] map, final List<String> instructions) {
+        final List<Coordinate> boxes = ArrayUtils.findAllOccurences(map, "[");
+        final Coordinate start = ArrayUtils.findAllOccurences(map, "@").get(0);
+        final List<Coordinate> frames = ArrayUtils.findAllOccurences(map, "#");
+
+        final List<List<Coordinate>> part2Boxes = boxes.stream()
+                .map(box -> List.of(box, new Coordinate(box.getX(), box.getY() + 1)))
+                .toList();
+
+        instructions.forEach(instruction -> {
+            move(start, part2Boxes, frames, parseDirection(instruction));
+            log("Instruction: " + instruction);
+            if (getLoggingEnabled()) {
+                createAndLogMap(start, part2Boxes, frames, map);
+            }
+        });
+
+        return boxes;
+    }
+
+    private Pair<Integer, Integer> parseDirection(final String instruction) {
+        return switch (instruction) {
+            case "<" -> Pair.of(0, -1);
+            case ">" -> Pair.of(0, 1);
+            case "v" -> Pair.of(1, 0);
+            case "^" -> Pair.of(-1, 0);
+            default -> Pair.of(0, 0);
+        };
+    }
+
+    private void move(final Coordinate start, final List<List<Coordinate>> boxes, final List<Coordinate> frames, final Pair<Integer, Integer> direction) {
+        final Queue<List<Coordinate>> coordsToCheck = new ArrayDeque<>();
+        coordsToCheck.add(List.of(start));
+        final Set<List<Coordinate>> objectsToMove = new HashSet<>();
+        objectsToMove.add(List.of(start));
+
+        while (!coordsToCheck.isEmpty()) {
+            final List<Coordinate> object = coordsToCheck.poll();
+            for (final Coordinate coord : object) {
+                final int newX = coord.getX() + direction.getLeft();
+                final int newY = coord.getY() + direction.getRight();
+
+                if (frames.contains(new Coordinate(newX, newY))) {
+                    return;
+                }
+
+                boxes.forEach(box -> {
+                    if (box.contains(new Coordinate(newX, newY)) && objectsToMove.add(box)) {
+                        coordsToCheck.add(box);
+                    }
+                });
+            }
+        }
+
+        objectsToMove.forEach(object -> object.forEach(coord -> {
+            coord.setX(coord.getX() + direction.getLeft());
+            coord.setY(coord.getY() + direction.getRight());
+        }));
+    }
+
+    private void createAndLogMap(
+            final Coordinate start,
+            final List<List<Coordinate>> boxes,
+            final List<Coordinate> frames,
+            final String[][] map
+    ) {
+        final String[][] tmpMap = new String[map.length][map[0].length];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                tmpMap[i][j] = ".";
+
+                for (final List<Coordinate> box : boxes) {
+                    for (final Coordinate boxTile : box) {
+                        if (boxTile.x == i && boxTile.y == j) {
+                            tmpMap[i][j] = "O";
+                            break;
+                        }
+                    }
+
+                }
+
+                for (final Coordinate frame : frames) {
+                    if (frame.x == i && frame.y == j) {
+                        tmpMap[i][j] = "#";
+                        break;
+                    }
+                }
+
+                if (start.x == i && start.y == j) {
+                    tmpMap[i][j] = "@";
+                }
+
+
+            }
+
+
+        }
+
+        logMap(tmpMap);
+    }
+
+    private Pair<String[][], List<String>> parseInput(final List<String> importInput) {
+        final List<String> mapLines = new ArrayList<>();
+        final StringBuilder instructions = new StringBuilder();
+
+        importInput.forEach(line -> {
+            if (line.isEmpty()) return;
+            if (line.matches("[<>v^]+")) {
+                instructions.append(line);
+            } else {
+                mapLines.add(line);
+            }
+        });
+
+        return Pair.of(ImportUtils.convertListToArray(mapLines), List.of(instructions.toString().split("")));
+    }
+
+
 }
